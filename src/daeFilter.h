93fe01839e3f468bd581cf4575817aba8130c9a4
Sébastien Barthélémy <barthelemy@crans.org>
2010-09-30 09:17:56 +0200

    daeFilter.h: removed trailing white spaces

--- src/daeFilter.h
+++ src/daeFilter.h
@@ -11,7 +11,7 @@
 #include <vector>
 #include <time.h>
 
-#include "dae.h" 
+#include "dae.h"
 #include "dae/daeSIDResolver.h"
 #include "dae/daeErrorHandler.h"
 #include "dom/domTypes.h"
@@ -24,53 +24,40 @@
 #include "dom/domProfile_COMMON.h"
 #include "dom/domFx_include_common.h"
 
-
-
-
 #ifndef MAX_PATH
 #define MAX_PATH 1024
 #endif
 #define MAX_LOG_BUFFER 1024
-#define MAX_NAME_SIZE 512 
-
-
-
+#define MAX_NAME_SIZE 512
 
 #ifndef stricmp
 inline int stricmp(const char *s1, const char *s2)
 {
 	char f, l;
-	
-	do 
+
+	do
 	{
 		f = ((*s1 <= 'Z') && (*s1 >= 'A')) ? *s1 + 'a' - 'A' : *s1;
 		l = ((*s2 <= 'Z') && (*s2 >= 'A')) ? *s2 + 'a' - 'A' : *s2;
 		s1++;
 		s2++;
 	} while ((f) && (f == l));
-	
+
 	return (int) (f - l);
 }
 
-#endif 
-
-
-
+#endif
 
 class CoherencyTestErrorHandler : public daeErrorHandler
 {
 public:
 	CoherencyTestErrorHandler();
 	virtual ~CoherencyTestErrorHandler();
-	
+
 public:
 	void handleError( daeString msg );
 	void handleWarning( daeString msg );
 };
-
-
-
-
 
 #endif
 
