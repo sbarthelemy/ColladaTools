844eead2c31efc6df82635727d2a8a9c65737088
Sébastien Barthélémy <sbarthelemy@aldebaran-robotics.com>
2013-03-11 11:57:42 +0100

    reindent avreader

--- avreader/avreader/avreader.h
+++ avreader/avreader/avreader.h
@@ -4,75 +4,75 @@
 #include <map>
 
 namespace logging {
-    static const bool debug = true;
-    static const bool info = true;
-    static const bool warning = true;
-    static const bool error = true;
+static const bool debug = true;
+static const bool info = true;
+static const bool warning = true;
+static const bool error = true;
 }
 
 namespace av {
 
-    class Matrix {
-    private:
-        double data[16];
-    public:
-        Matrix ();
-        Matrix (double v0, double v1, double v2, double v3,
-                double v4, double v5, double v6, double v7,
-                double v8, double v9, double v10, double v11,
-                double v12, double v13, double v14, double v15);
-        Matrix (const double val[16]);
-        double operator[] (const std::size_t index) const;
-    };
+class Matrix {
+private:
+  double data[16];
+public:
+  Matrix ();
+  Matrix (double v0, double v1, double v2, double v3,
+          double v4, double v5, double v6, double v7,
+          double v8, double v9, double v10, double v11,
+          double v12, double v13, double v14, double v15);
+  Matrix (const double val[16]);
+  double operator[] (const std::size_t index) const;
+};
 
-    std::ostream& operator<< (std::ostream& os,const Matrix m);
+std::ostream& operator<< (std::ostream& os,const Matrix m);
 
-    class Translate {
-    private:
-        double data[3];
-    public:
-        Translate ();
-        Translate (double v0, double v1, double v2);
-        Translate (const double val[3]);
-        double operator[] (const std::size_t index) const;
-        operator Matrix () const;
-    };
+class Translate {
+private:
+  double data[3];
+public:
+  Translate ();
+  Translate (double v0, double v1, double v2);
+  Translate (const double val[3]);
+  double operator[] (const std::size_t index) const;
+  operator Matrix () const;
+};
 
-    std::ostream& operator<< (std::ostream& os, const Translate t);
+std::ostream& operator<< (std::ostream& os, const Translate t);
 
-    class Wrench{
-    private:
-        double data[6];
-    public:
-        Wrench();
-        Wrench(const double val[6]);
-        const double& operator[] (const std::size_t index);
-    };
+class Wrench{
+private:
+  double data[6];
+public:
+  Wrench();
+  Wrench(const double val[6]);
+  const double& operator[] (const std::size_t index);
+};
 
-    std::ostream& operator<<(std::ostream& os, const Wrench w);
+std::ostream& operator<<(std::ostream& os, const Wrench w);
 
-    class FrameData{
-    public:
-        unsigned long step;
-        std::map <std::string, Matrix >     matrices;
-        std::map <std::string, Translate >  translates;
-        std::map <std::string, Wrench >     wrenches;
-    };
+class FrameData{
+public:
+  unsigned long step;
+  std::map <std::string, Matrix >     matrices;
+  std::map <std::string, Translate >  translates;
+  std::map <std::string, Wrench >     wrenches;
+};
 
-    std::ostream& operator<< (std::ostream& os, const FrameData fd);
+std::ostream& operator<< (std::ostream& os, const FrameData fd);
 
-    class RandomReader {
-    public:
-        virtual ~RandomReader() {};
-        virtual FrameData getFrame(unsigned long step) const = 0;
-    };
+class RandomReader {
+public:
+  virtual ~RandomReader() {};
+  virtual FrameData getFrame(unsigned long step) const = 0;
+};
 
-    class LastReader {
-    public:
-        virtual ~LastReader() {};
-        virtual void start() = 0;
-        virtual void stop() = 0;
-        virtual FrameData getLatestFrame() = 0;
-    };
+class LastReader {
+public:
+  virtual ~LastReader() {};
+  virtual void start() = 0;
+  virtual void stop() = 0;
+  virtual FrameData getLatestFrame() = 0;
+};
 }
 #endif
--- avreader/avreader/avreaderh5.h
+++ avreader/avreader/avreaderh5.h
@@ -16,42 +16,42 @@
 #include <avreader/avreader.h>
 
 namespace av {
-    class InvalidFileException : public std::runtime_error {
-    public:
-       InvalidFileException(): std::runtime_error("Invalid scene file") {};
-       InvalidFileException(const std::string msg): std::runtime_error(msg) {};
-    };
+class InvalidFileException : public std::runtime_error {
+public:
+ InvalidFileException(): std::runtime_error("Invalid scene file") {};
+ InvalidFileException(const std::string msg): std::runtime_error(msg) {};
+};
 
-    class H5RandomReader : public RandomReader {
-    public:
-        H5RandomReader(const std::string fileName, const std::string GroupPath)
-            throw (InvalidFileException);
-        ~H5RandomReader();
-        FrameData getFrame(unsigned long step) const;
-        unsigned long getNbSteps() const {return nSteps;};
-        double getTime(unsigned long step) {return timedata[step];}
+class H5RandomReader : public RandomReader {
+public:
+  H5RandomReader(const std::string fileName, const std::string GroupPath)
+      throw (InvalidFileException);
+  ~H5RandomReader();
+  FrameData getFrame(unsigned long step) const;
+  unsigned long getNbSteps() const {return nSteps;};
+  double getTime(unsigned long step) {return timedata[step];}
 
-    private:
-        unsigned long nSteps;
-        H5::H5File file;
-        H5::Group group, transformsGroup;
-        H5::DataSet timeline;
-        double *timedata;
-        std::map<std::string, H5::DataSet >  matrices;
-        std::map<std::string, H5::DataSet >  translates;
-        std::map<std::string, H5::DataSet >  wrenches;
-    };
+private:
+  unsigned long nSteps;
+  H5::H5File file;
+  H5::Group group, transformsGroup;
+  H5::DataSet timeline;
+  double *timedata;
+  std::map<std::string, H5::DataSet >  matrices;
+  std::map<std::string, H5::DataSet >  translates;
+  std::map<std::string, H5::DataSet >  wrenches;
+};
 
-    class H5LastReader : public LastReader {
-    public:
-        H5LastReader(const std::string fileName, const std::string GroupPath);
-        FrameData getLatestFrame();
-        void start() {};
-        void stop() {lastStep = 0;};
+class H5LastReader : public LastReader {
+public:
+  H5LastReader(const std::string fileName, const std::string GroupPath);
+  FrameData getLatestFrame();
+  void start() {};
+  void stop() {lastStep = 0;};
 
-    private:
-        unsigned long lastStep;
-        H5RandomReader reader;
-    };
+private:
+  unsigned long lastStep;
+  H5RandomReader reader;
+};
 }
 #endif
