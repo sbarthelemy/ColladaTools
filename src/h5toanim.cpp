43160ceccc6c9102c282f334c4259f18567d6994
Sébastien Barthélémy <sbarthelemy@aldebaran-robotics.com>
2013-03-15 21:19:24 +0100

    h5toanim fix style

--- src/h5toanim.cpp
+++ src/h5toanim.cpp
@@ -15,31 +15,31 @@ using namespace av;
 class SourceExporter
 {
 private :
-  char *mName;
-  int mCount, mStride;
-  domAnimation *mRoot;
-  domSource *mSource;
-  domFloat_array *mFA;
-  domName_array *mNA;
-  domTechnique *mTech;
-  domAccessor *mAccess;
+  char *name_;
+  int count_, stride_;
+  domAnimation *root_;
+  domSource *source_;
+  domFloat_array *float_array_;
+  domName_array *name_array_;
+  domTechnique *technique_;
+  domAccessor *accessor_;
 
 public :
   SourceExporter(domAnimation *root, int stride, int isName, const char *name, const char *type);
   ~SourceExporter();
-  void add(float val);
-  void add(float val0, float val1, float val2);
-  void add(float val0, float val1, float val2, float val3,
+  void Add(float val);
+  void Add(float val0, float val1, float val2);
+  void Add(float val0, float val1, float val2, float val3,
            float val4, float val5, float val6, float val7,
            float val8, float val9, float val10, float val11,
            float val12, float val13, float val14, float val15);
-  void add(const char *val);
-  void add(const char *val0, const char *val1, const char *val2);
-  void add(const char *val0, const char *val1, const char *val2, const char *val3,
+  void Add(const char *val);
+  void Add(const char *val0, const char *val1, const char *val2);
+  void Add(const char *val0, const char *val1, const char *val2, const char *val3,
            const char *val4, const char *val5, const char *val6, const char *val7,
            const char *val8, const char *val9, const char *val10, const char *val11,
            const char *val12, const char *val13, const char *val14, const char *val15);
-  void addAccessorParam(const char *name, const char *type);
+  void AddAccessorParam(const char *name, const char *type);
 };
 
 SourceExporter::SourceExporter(domAnimation *root, int stride, int isName, const char *name, const char *type)
@@ -48,101 +48,101 @@ SourceExporter::SourceExporter(domAnimation *root, int stride, int isName, const
   char tmp2[1024];
   char buffer[32];
   sprintf(tmp, "%s-%s", name, type);
-  mName = strdup(tmp);
-  mRoot = root;
-  mStride = stride;
+  name_ = strdup(tmp);
+  root_ = root;
+  stride_ = stride;
 
-  mSource = (domSource*)mRoot->add("source");
-  mSource->setId(tmp);
+  source_ = (domSource*)root_->add("source");
+  source_->setId(tmp);
   strcat(tmp, "-array");
 
   if (isName) {
-    mNA = (domName_array*)mSource->add("Name_array");
-    mNA->setId(tmp);
+    name_array_ = (domName_array*)source_->add("Name_array");
+    name_array_->setId(tmp);
   } else {
-    mFA = (domFloat_array*)mSource->add("float_array");
-    mFA->setId(tmp);
+    float_array_ = (domFloat_array*)source_->add("float_array");
+    float_array_->setId(tmp);
   }
 
-  mTech = (domTechnique*)mSource->add("technique_common");
-  mAccess = (domAccessor*)mTech->add("accessor");
+  technique_ = (domTechnique*)source_->add("technique_common");
+  accessor_ = (domAccessor*)technique_->add("accessor");
 
   sprintf(tmp2, "#%s", tmp);
-  mAccess->setSource(tmp2);
-  mAccess->setStride(mStride);
+  accessor_->setSource(tmp2);
+  accessor_->setStride(stride_);
 
-  mCount = 0;
+  count_ = 0;
 }
 
 SourceExporter::~SourceExporter()
 {
-  free(mName);
+  free(name_);
 }
 
-void SourceExporter::add(float val)
+void SourceExporter::Add(float val)
 {
-  mFA->getValue().append(val);
-  mCount++;
-  mFA->setCount(mCount);
-  mAccess->setCount(mCount/mStride);
+  float_array_->getValue().append(val);
+  count_++;
+  float_array_->setCount(count_);
+  accessor_->setCount(count_/stride_);
 }
 
-void SourceExporter::add(float val0, float val1, float val2)
+void SourceExporter::Add(float val0, float val1, float val2)
 {
-  mFA->getValue().append3(val0, val1, val2);
-  mCount+=3;
-  mFA->setCount(mCount);
-  mAccess->setCount(mCount/mStride);
+  float_array_->getValue().append3(val0, val1, val2);
+  count_+=3;
+  float_array_->setCount(count_);
+  accessor_->setCount(count_/stride_);
 }
 
-void SourceExporter::add(float val0, float val1, float val2, float val3,
+void SourceExporter::Add(float val0, float val1, float val2, float val3,
                          float val4, float val5, float val6, float val7,
                          float val8, float val9, float val10, float val11,
                          float val12, float val13, float val14, float val15)
 {
-  mFA->getValue().append4(val0, val1, val2, val3);
-  mFA->getValue().append4(val4, val5, val6, val7);
-  mFA->getValue().append4(val8, val9, val10, val11);
-  mFA->getValue().append4(val12, val13, val14, val15);
-  mCount+=16;
-  mFA->setCount(mCount);
-  mAccess->setCount(mCount/mStride);
+  float_array_->getValue().append4(val0, val1, val2, val3);
+  float_array_->getValue().append4(val4, val5, val6, val7);
+  float_array_->getValue().append4(val8, val9, val10, val11);
+  float_array_->getValue().append4(val12, val13, val14, val15);
+  count_+=16;
+  float_array_->setCount(count_);
+  accessor_->setCount(count_/stride_);
 }
 
-void SourceExporter::add(const char *val)
+void SourceExporter::Add(const char *val)
 {
-  mNA->getValue().append(val);
-  mCount++;
-  mNA->setCount(mCount);
-  mAccess->setCount(mCount/mStride);
+  name_array_->getValue().append(val);
+  count_++;
+  name_array_->setCount(count_);
+  accessor_->setCount(count_/stride_);
 }
 
-void SourceExporter::add(const char *val0, const char *val1, const char *val2)
+void SourceExporter::Add(const char *val0, const char *val1, const char *val2)
 {
-  mNA->getValue().append3(val0, val1, val2);
-  mCount += 3;
-  mNA->setCount(mCount);
-  mAccess->setCount(mCount/mStride);
+  name_array_->getValue().append3(val0, val1, val2);
+  count_ += 3;
+  name_array_->setCount(count_);
+  accessor_->setCount(count_/stride_);
 }
 
-void SourceExporter::add(const char *val0, const char *val1, const char *val2, const char *val3,
+void SourceExporter::Add(const char *val0, const char *val1, const char *val2, const char *val3,
                          const char *val4, const char *val5, const char *val6, const char *val7,
                          const char *val8, const char *val9, const char *val10, const char *val11,
                          const char *val12, const char *val13, const char *val14, const char *val15)
 {
-  mNA->getValue().append4(val0, val1, val2, val3);
-  mNA->getValue().append4(val4, val5, val6, val7);
-  mNA->getValue().append4(val8, val9, val10, val11);
-  mNA->getValue().append4(val12, val13, val14, val15);
-  mCount += 16;
-  mNA->setCount(mCount);
-  mAccess->setCount(mCount/mStride);
+  name_array_->getValue().append4(val0, val1, val2, val3);
+  name_array_->getValue().append4(val4, val5, val6, val7);
+  name_array_->getValue().append4(val8, val9, val10, val11);
+  name_array_->getValue().append4(val12, val13, val14, val15);
+  count_ += 16;
+  name_array_->setCount(count_);
+  accessor_->setCount(count_/stride_);
 }
 
 
-void SourceExporter::addAccessorParam(const char *name, const char *type)
+void SourceExporter::AddAccessorParam(const char *name, const char *type)
 {
-  domParam *param = (domParam*)mAccess->add("param");
+  domParam *param = (domParam*)accessor_->add("param");
   param->setName(name);
   param->setType(type);
 }
@@ -151,20 +151,20 @@ void SourceExporter::addAccessorParam(const char *name, const char *type)
 class AnimationExporter
 {
 private :
-  char *mName;
-  domAnimation *mAnim;
-  domLibrary_animations *mRoot;
-  SourceExporter *mInput, *mInterpolation, *mOutput;
-  domSampler *mSampler;
+  char *name_;
+  domAnimation *anim_;
+  domLibrary_animations *root_;
+  SourceExporter *input_, *interpolation_, *output_;
+  domSampler *sampler_;
 
-  void addInput(const char *semantic, const char *source);
+  void AddInput(const char *semantic, const char *source);
 
 public :
   AnimationExporter(domLibrary_animations *root, int stride, const char *name, const char *type);
   ~AnimationExporter();
-  void add(float timestamp, float val);
-  void add(float timestamp, float val0, float val1, float val2);
-  void add(float timestamp,
+  void Add(float timestamp, float val);
+  void Add(float timestamp, float val0, float val1, float val2);
+  void Add(float timestamp,
            float val0, float val1, float val2, float val3,
            float val4, float val5, float val6, float val7,
            float val8, float val9, float val10, float val11,
@@ -178,36 +178,36 @@ AnimationExporter::AnimationExporter(domLibrary_animations *root, int stride, co
   char buffer[1024];
   char target[1024];
   sprintf(tmp, "%s-%s", name, type);
-  mName = strdup(tmp);
-  mRoot = root;
+  name_ = strdup(tmp);
+  root_ = root;
 
-  mAnim = (domAnimation*)mRoot->add("animation");
-  mAnim->setId(mName);
+  anim_ = (domAnimation*)root_->add("animation");
+  anim_->setId(name_);
 
-  mInput = new SourceExporter(mAnim, 1, 0, tmp, "input");
-  mInterpolation = new SourceExporter(mAnim, 1, 1, tmp, "interpolation");
-  mOutput = new SourceExporter(mAnim, stride, 0, tmp, "output");
+  input_ = new SourceExporter(anim_, 1, 0, tmp, "input");
+  interpolation_ = new SourceExporter(anim_, 1, 1, tmp, "interpolation");
+  output_ = new SourceExporter(anim_, stride, 0, tmp, "output");
 
   // Ajouter les parametres...
-  mInput->addAccessorParam("TIME", "float");
+  input_->AddAccessorParam("TIME", "float");
 
   if (stride == 3){
     const char *letters[3] = {"X", "Y", "Z"};
     for (int i=0; i<3; i++){
-        mInterpolation->addAccessorParam(letters[i], "Name");
-        mOutput->addAccessorParam(letters[i], "float");
+        interpolation_->AddAccessorParam(letters[i], "Name");
+        output_->AddAccessorParam(letters[i], "float");
     }
     // Target ?
     sprintf(target, "%s/%s", name, type);
   } else if (stride == 1) {
-    mInterpolation->addAccessorParam("INTERPOLATION", "Name");
-    mOutput->addAccessorParam("ANGLE", "float");
+    interpolation_->AddAccessorParam("INTERPOLATION", "Name");
+    output_->AddAccessorParam("ANGLE", "float");
 
     // Target ?
     sprintf(target, "%s/%s.ANGLE", name, type);
   } else if (stride == 16) {
-    mInterpolation->addAccessorParam("INTERPOLATION", "Name");
-    mOutput->addAccessorParam("TRANSFORM", "float4x4");
+    interpolation_->AddAccessorParam("INTERPOLATION", "Name");
+    output_->AddAccessorParam("TRANSFORM", "float4x4");
 
     // Target ?
     sprintf(target, "%s/%s", name, type);
@@ -215,14 +215,14 @@ AnimationExporter::AnimationExporter(domLibrary_animations *root, int stride, co
 
 
   sprintf(buffer, "%s-sampler", tmp);
-  mSampler = (domSampler*)mAnim->add("sampler");
-  mSampler->setId(buffer);
+  sampler_ = (domSampler*)anim_->add("sampler");
+  sampler_->setId(buffer);
 
-  addInput("INPUT", "input");
-  addInput("OUTPUT", "output");
-  addInput("INTERPOLATION", "interpolation");
+  AddInput("INPUT", "input");
+  AddInput("OUTPUT", "output");
+  AddInput("INTERPOLATION", "interpolation");
 
-  domChannel *chan = (domChannel*)mAnim->add("channel");
+  domChannel *chan = (domChannel*)anim_->add("channel");
   sprintf(buffer, "#%s-sampler", tmp);
   chan->setSource(buffer);
   chan->setTarget(target);
@@ -230,48 +230,48 @@ AnimationExporter::AnimationExporter(domLibrary_animations *root, int stride, co
 
 AnimationExporter::~AnimationExporter()
 {
-  free(mName);
+  free(name_);
 }
 
-void AnimationExporter::addInput(const char *semantic, const char *source)
+void AnimationExporter::AddInput(const char *semantic, const char *source)
 {
   char buffer[1024];
-  domInputLocal *i = (domInputLocal*)mSampler->add("input");
+  domInputLocal *i = (domInputLocal*)sampler_->add("input");
 
-  sprintf(buffer, "#%s-%s", mName, source);
+  sprintf(buffer, "#%s-%s", name_, source);
   i->setSemantic(semantic);
   i->setSource(buffer);
 }
 
 
-void AnimationExporter::add(float timestamp, float val)
+void AnimationExporter::Add(float timestamp, float val)
 {
-  mInput->add(timestamp);
-  mInterpolation->add("STEP");
-  mOutput->add(val);
+  input_->Add(timestamp);
+  interpolation_->Add("STEP");
+  output_->Add(val);
 }
 
-void AnimationExporter::add(float timestamp, float val0, float val1, float val2)
+void AnimationExporter::Add(float timestamp, float val0, float val1, float val2)
 {
-  mInput->add(timestamp);
-  mInterpolation->add("STEP", "STEP", "STEP");
-  mOutput->add(val0, val1, val2);
+  input_->Add(timestamp);
+  interpolation_->Add("STEP", "STEP", "STEP");
+  output_->Add(val0, val1, val2);
 }
 
-void AnimationExporter::add(float timestamp,
+void AnimationExporter::Add(float timestamp,
                             float val0, float val1, float val2, float val3,
                             float val4, float val5, float val6, float val7,
                             float val8, float val9, float val10, float val11,
                             float val12, float val13, float val14, float val15)
 {
-  mInput->add(timestamp);
+  input_->Add(timestamp);
 /*  mInterpolation->add("STEP", "STEP", "STEP", "STEP",
                         "STEP", "STEP", "STEP", "STEP",
                         "STEP", "STEP", "STEP", "STEP",
                         "STEP", "STEP", "STEP", "STEP");
 */
-  mInterpolation->add("STEP");
-  mOutput->add(val0, val1, val2, val3,
+  interpolation_->Add("STEP");
+  output_->Add(val0, val1, val2, val3,
                val4, val5, val6, val7,
                val8, val9, val10, val11,
                val12, val13, val14, val15);
@@ -320,50 +320,35 @@ int main(int argc, char **argv )
 
   std::vector<std::string> elements;
   DAE *dae = new DAE;
-  //daeElement *root = (daeElement*)dae->open(scene_file);
-
-  domCOLLADA *root = (domCOLLADA*)dae->open(scene_file);
+  domCOLLADA *root = dae->open(scene_file);
 
   boost::scoped_ptr<RandomReader> myReader(MakeRandomReader(hdf5_file,
                                                             hdf5_group));
 
-  FrameData fd = myReader->getFrame(0);
-  std::map <std::string, TransformMatrix >::iterator it;
-  for (it = fd.tranform_matrices.begin(); it != fd.tranform_matrices.end(); ++it) {
-    elements.push_back(std::string(it->first));
-  }
-  //printf("Found %i elements... \n", elements.size() );
-
-  domLibrary_animations *lib_anim;
-
-  if (elements.size()!=0) {
-      lib_anim = (domLibrary_animations*)root->add("library_animations");
-  }
-
-  //printf("Found %i steps...\n", (int)myReader->getNbSteps());
-
-  for (int j=0; j<elements.size(); j++) {
-    AnimationExporter *matrix = new AnimationExporter(lib_anim, 16, elements[j].c_str(), "matrix");
-
-    for (int i=0; i<myReader->getNbSteps(); i++) {
-      //printf("Step %i\n", i);
-      fd = myReader->getFrame(i);
-
-      std::map <std::string, TransformMatrix >::iterator it;
-      for (it = fd.tranform_matrices.begin(); it != fd.tranform_matrices.end(); ++it){
-        if (elements[j] == (it->first).c_str()) {
-          //std::cout << "Key : " << it->first << std::endl;
-          //td::cout << ax << "\t" << ay << "\t" << az << std::endl;
-          matrix->add(myReader->getTime(i),
-              it->second[0 + 0*4], it->second[1 + 0*4], it->second[2 + 0*4], it->second[3 + 0*4],
-              it->second[0 + 1*4], it->second[1 + 1*4], it->second[2 + 1*4], it->second[3 + 1*4],
-              it->second[0 + 2*4], it->second[1 + 2*4], it->second[2 + 2*4], it->second[3 + 2*4],
-              it->second[0 + 3*4], it->second[1 + 3*4], it->second[2 + 3*4], it->second[3 + 3*4]);
-        }
+  FrameData ffd = myReader->getFrame(0);
+  if (!ffd.tranform_matrices.empty() ||
+      !ffd.rigid_tranform_matrices.empty() ||
+      !ffd.translations.empty()) {
+    domLibrary_animations *lib_anim =
+        (domLibrary_animations*)root->add("library_animations");
+    for (std::map <std::string, TransformMatrix>::iterator it
+             = ffd.tranform_matrices.begin();
+         it != ffd.tranform_matrices.end();
+         ++it) {
+      AnimationExporter exporter(lib_anim, 16, it->first.c_str(), "matrix");
+      for (int i=0; i<myReader->getNbSteps(); ++i) {
+        // Note: this is very inefficient: we copy the whole map but use a
+        // single element
+        FrameData fd = myReader->getFrame(i);
+        TransformMatrix& data = fd.tranform_matrices[it->first];
+        exporter.Add(myReader->getTime(i),
+            data[0 + 0*4], data[1 + 0*4], data[2 + 0*4], data[3 + 0*4],
+            data[0 + 1*4], data[1 + 1*4], data[2 + 1*4], data[3 + 1*4],
+            data[0 + 2*4], data[1 + 2*4], data[2 + 2*4], data[3 + 2*4],
+            data[0 + 3*4], data[1 + 3*4], data[2 + 3*4], data[3 + 3*4]);
       }
     }
   }
-
   dae->add(file_out);
   dae->setRoot(file_out, (domCOLLADA*)root);
   dae->write(file_out);
